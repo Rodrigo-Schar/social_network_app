88f32a89e56fbd732c36677307e439db2d6afff6
Rodrigo-Schar <Rodrigo.Schar@jala-foundation.org>
2022-10-10 16:56:09 -0500

    test bitwise

--- social_network_app/modules/auth/login/controllers/LoginViewController.swift
+++ social_network_app/modules/auth/login/controllers/LoginViewController.swift
@@ -19,7 +19,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpKeyboardNotification()
-        // Testing bitrise and firebase distribution
+        // Testing bitrise and firebase distribution ios
     }
     
     func setUpKeyboardNotification() {
